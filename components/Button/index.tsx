f3550602d8d6e870581ace5282e91c3c3f2f1aef
Maykon Morais <maykon.silva@compasso.com.br>
2023-10-09 14:40:29 -0300

    initial test with emotion style

---
+++ components/Button/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import ButtonTest from "./styles";
+
+const Button: React.FC = () => {
+  return <ButtonTest>LOGIN</ButtonTest>;
+};
+
+export default Button;
--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
         filename: "static/chunks/remoteEntry.js",
         exposes: {},
         shared: {},
-      }),
+      })
     );
 
     return config;
